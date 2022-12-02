7825dec0fc8c4e98f1b51e881eab308f874a4bc9
ismail AbdulHadi <95002463+CodingClu8@users.noreply.github.com>
2022-12-02 19:58:36 +0200

    geese solution

---
+++ basic-variable-assignment.js
@@ -0,0 +1,21 @@
+//This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+//The code
+
+/*
+
+var a == "code";
+var b == "wa.rs";
+var name == a + b;
+
+*/
+
+
+/*   my solution
+
+
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
+
+*/
