2b5e88dbf786c93bceb2868dc4e124575038d10e
ismail AbdulHadi <95002463+CodingClu8@users.noreply.github.com>
2022-12-08 21:14:12 +0200

    pre fizzbuzz solution

---
+++ Pre-FizzBuzz Workout #1.js
@@ -0,0 +1,19 @@
+/* the problem
+
+
+
+*/
+
+
+
+//my solution
+
+
+let output = new Array();
+let num = 1;
+
+while(output.length < n){
+ output.push(num)
+ num += 1;
+}
+return output;
--- Welcome!.js
+++ Welcome!.js
@@ -16,21 +16,21 @@ greet = lan =>
 
 (hello => hello[lan] || 'Welcome')
 ({
-    czech: 'Vitejte',
-    danish: 'Velkomst',
-    dutch: 'Welkom',
-    estonian: 'Tere tulemast',
-    finnish: 'Tervetuloa',
-    flemish: 'Welgekomen',
-    french: 'Bienvenue',
-    german: 'Willkommen',
-    irish: 'Failte',
-    italian: 'Benvenuto',
-    latvian: 'Gaidits',
-    lithuanian: 'Laukiamas',
-    polish: 'Witamy',
-    spanish: 'Bienvenido',
-    swedish: 'Valkommen',
-    welsh: 'Croeso'
-     
+     czech: 'Vitejte',
+danish: 'Velkomst',
+dutch: 'Welkom',
+estonian: 'Tere tulemast',
+finnish: 'Tervetuloa',
+flemish: 'Welgekomen',
+french: 'Bienvenue',
+german: 'Willkommen',
+irish: 'Failte',
+italian: 'Benvenuto',
+latvian: 'Gaidits',
+lithuanian: 'Laukiamas',
+polish: 'Witamy',
+spanish: 'Bienvenido',
+swedish: 'Valkommen',
+welsh: 'Croeso'
+ 
 })
