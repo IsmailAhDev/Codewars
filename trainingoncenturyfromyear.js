d50dffedfed5696ff5657d508e8e8c98e7ee15c8
ismail AbdulHadi <95002463+CodingClu8@users.noreply.github.com>
2022-12-17 20:13:02 +0200

    century solution

---
+++ trainingoncenturyfromyear.js
@@ -0,0 +1,27 @@
+/*
+The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+Task
+Given a year, return the century it is in.
+
+*/
+
+
+//my soltuion
+
+
+function century(year) {
+  
+  //given a number taht represents a year return the current century in number form
+  //first check the year
+  // build a function that will return the century
+  
+  
+  //I probably will have to do a math function
+  
+  
+  return Math.ceil(year/100);
+  
+  
+  return;
+}
