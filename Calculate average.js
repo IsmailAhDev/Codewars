f90bde4d77aed004dbbe9bc6c35d9949b302683a
ismail AbdulHadi <95002463+CodingClu8@users.noreply.github.com>
2022-12-22 10:47:30 +0200

    calculate average solution

---
+++ Calculate average.js
@@ -0,0 +1,20 @@
+/*
+
+
+Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0.
+
+
+
+
+
+*/
+
+
+//my solution
+
+
+
+
+const findAverage = array => array.length == 0 ? 0 : array.reduce((prev,curr) => prev + curr) / array.length
