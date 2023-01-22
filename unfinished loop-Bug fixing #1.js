9ba67bcbcd9049b7b130d996dbba6a6d17426d1e
ismail AbdulHadi <95002463+CodingClu8@users.noreply.github.com>
2023-01-22 15:50:15 +0200

    unfinished loop-bug  fixing #1.js solution

---
+++ unfinished loop-Bug fixing #1.js
@@ -0,0 +1,16 @@
+/* the problem */ /*Unfinished Loop - Bug Fixing #1
+Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
+*/ 
+
+//the solution
+
+
+function createArray(number){
+    var newArray = [];
+    
+    for(var counter = 1; counter <= number; counter++){
+      newArray.push(counter);
+    }
+    
+    return newArray;
+  }
