767186ccc31a795c135d19cbed1bb0fa9c49da99
ismail AbdulHadi <95002463+CodingClu8@users.noreply.github.com>
2023-02-15 17:39:05 +0200

    grasshopperfunction syntax debugging solution

---
+++ GrassHopperfunction syntax debugging.js
@@ -0,0 +1,15 @@
+/* the problem
+
+
+
+Grasshopper - Function syntax debugging
+A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+
+
+ the solution /*
+
+
+ function main (verb, noun){
+  return verb + noun
+}
