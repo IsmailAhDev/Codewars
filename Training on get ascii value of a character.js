94cbc7177a0a6291deab9667d6973694e85b7c78
ismail AbdulHadi <95002463+CodingClu8@users.noreply.github.com>
2023-02-26 10:34:36 +0200

    training on get ascci value of a character solution

---
+++ Training on get ascii value of a character.js
@@ -0,0 +1,17 @@
+/* the problem
+
+
+Get ASCII value of a character.
+
+For the ASCII table you can refer to http://www.asciitable.com/
+
+
+
+
+the solution */
+
+function getASCII(c){
+    // ...
+    
+    return c.charCodeAt(c);
+  }
