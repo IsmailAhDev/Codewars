a277cdb8ebcd6838855d1fcf768cc50acb54e3a7
ismail AbdulHadi <95002463+CodingClu8@users.noreply.github.com>
2023-03-19 20:40:50 +0200

    Not so black box

---
+++ Training-On-Not-So-Black-Box.js
@@ -0,0 +1,19 @@
+/*    THE PROBLEM
+
+
+
+The kata has been preloaded with a function called blackBox. Figure out what the function does.
+
+You may need to think laterally!
+
+
+
+ 
+ THE SOLUTION     */
+
+
+
+ console.log(blackBox.toString());
+ JSopenSesame();
+ 
+ 
